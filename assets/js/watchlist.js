92b2f1ff20b74cb1e6347912f6391d020eebc795
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-05-18 17:42:42 +0100

    Created init function

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -1,5 +1,17 @@
 var returnBtn = document.getElementById('returnBtn')
 
+var watchList = []
+
+function init() {
+    var storedWatchList = JSON.parse(localStorage.getItem('watchList'))
+    
+    if (storedWatchList !== null) {
+        watchList = storedWatchList
+    }
+
+    displayWatchList()
+}
+
 // Takes user back to homepage
 returnBtn.addEventListener('click', function() {
     document.location.replace('index.html')
