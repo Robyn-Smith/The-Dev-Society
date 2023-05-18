91a130ca2edf66d69ffae806ee315142dcf13d66
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-05-18 17:34:59 +0100

    Added event listener to "Back to homepage" button

---
+++ assets/js/watchlist.js
@@ -0,0 +1,6 @@
+var returnBtn = document.getElementById('returnBtn')
+
+// Takes user back to homepage
+returnBtn.addEventListener('click', function() {
+    document.location.replace('index.html')
+})
