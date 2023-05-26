287eedd9c6c69f0efdce5ddadc7d8db8dff53925
percivalho <35653644+percivalho@users.noreply.github.com>
2023-05-26 09:23:03 +0100

    separte apikeys to keys.js

---
+++ assets/js/keys.js
@@ -0,0 +1,12 @@
+// OMDB API
+var apiKey = 'e53831fe';
+
+// YouTube API
+// Please swap the two if one is not working (probably due to quota limit)
+var apiKey2 = 'AIzaSyAQ6qYnLqpxfSWS1FZVakvqLEOfVjXlJp4';
+//var apiKey2 = 'AIzaSyDE6KlHz8PmrCyHMADxzBtkvSrMU2pzJ8c';
+
+// Google Map/Google GeoCode API
+var apiKey3 = 'AIzaSyCr-Av0kS8QAYgzV2dOHJXomDn8rxTcsRA';
+//var apiKey3a = '77ca7d6ec3122dd37ca00d73aa375bef';
+//var apiKey3 = 'AIzaSyD7xo_u2lBGkDL4xt_JdCr8ew06xLLt0pI';
